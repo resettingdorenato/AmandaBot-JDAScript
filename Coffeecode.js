f1ed69013c7b5688fd5dcffc3043b54b5b9eb369
resettingdorenato <148704333+resettingdorenato@users.noreply.github.com>
2023-10-22 12:10:02 -0300

    Create Coffeecode.js

---
+++ Coffeecode.js
@@ -0,0 +1,15 @@
+import JDA
+  import amandacoffee
+import Botdesignerfordiscord
+
+  I'm coding this.
+
+
+here's the code:
+
+import AMANDA_COOKIE_COUNT
+  from amandacookiecount
+import watercoolcount
+  import onioncount
+
+
